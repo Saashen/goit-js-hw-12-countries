2d305a86915275c0fde67406ce11c4ce2f50e2e8
Saashen <saashen.kyiv@gmail.com>
2019-11-24 19:20:11 +0200

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -14,60 +14,54 @@ const debounce = require('lodash.debounce');
 const refs = {
   inputForm: document.querySelector('#input-form'),
   countrySection: document.querySelector('#country'),
+  countriesSection: document.querySelector('#countries')
 };
 
 refs.inputForm.addEventListener('input', debounce(searchInputHandler, 1000));
 
 function searchInputHandler(e) {
+  e.preventDefault();
+
   const input = e.target;
   clearResult();
 
   spinner.show();
   fetchCountries(input.value)
-    .then(checkOne => {
-      if (checkOne.length <= 10) {
-        return checkOne;
-      } else {
-        const notice = PNotify.error({
-          text: 'Too many matches find. Please enter a more specific query',
-        });
-        return notice;
-      }
-    })
-    .then(checkTwo => {
-      if (checkTwo.length === 1) {
-        return checkTwo;
-      } else {
-        checkTwo.forEach(country => {
-          return insertCountriesMarkup(country);
-        });
-      }
-    })
     .then(countries => {
       spinner.hide();
-      countries.forEach(country => {
-        return insertCountryMarkup(country);
-      });
+      if (countries.length === 1) {
+        return insertCountryMarkup(countries);
+      } else if (countries.length > 1 && countries.length <= 10) {
+        return insertCountriesMarkup(countries);
+      } else {
+        PNotify.error({
+          text: 'Too many matches find. Please enter a more specific query',
+        });
+      }
     })
     .catch(error => {
       console.log(error);
     });
-
   input.value = '';
 }
 
-function insertCountriesMarkup(items) {
-  const markup = countriesShowTemplate(items);
-  refs.countrySection.insertAdjacentHTML('beforeend', markup);
-  refs.countrySection.classList.remove('is-hidden');
+function insertCountriesMarkup(countries) {
+  const markup = countries.map(country => {
+    return countriesShowTemplate(country);
+  }).join(' ');
+  refs.countriesSection.insertAdjacentHTML('beforeend', markup);
+  refs.countriesSection.classList.remove('is-hidden');
 }
 
-function insertCountryMarkup(item) {
-  const markup = countryShowTemplate(item);
+function insertCountryMarkup(countries) {
+  const markup = countries.map(country => {
+    return countryShowTemplate(country);
+  });
   refs.countrySection.insertAdjacentHTML('beforeend', markup);
   refs.countrySection.classList.remove('is-hidden');
 }
 
 function clearResult() {
   refs.countrySection.innerHTML = '';
+  refs.countriesSection.innerHTML = '';
 }
